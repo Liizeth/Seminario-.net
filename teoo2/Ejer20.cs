9b26e564f4489111ff4235f45eecf29c232d733d
liz <112222841+Liizeth@users.noreply.github.com>
2026-04-12 20:56:17 -0300

    Delete teoo2/Ejer20.cs

--- teoo2/Ejer20.cs
+++
@@ -1,8 +0,0 @@
-public class Ejer20
-{
-    public static void Ejecutar()
-    {
-        //Codificar el método Swap que recibe 2 parámetros enteros e intercambia sus valores. El cambio
-        //debe apreciarse en el método invocador.
-    }
-}
