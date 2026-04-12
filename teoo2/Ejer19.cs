d83cdbdcb625fdb7f97f8e30355677b805d93876
liz <112222841+Liizeth@users.noreply.github.com>
2026-04-12 20:56:46 -0300

    Delete teoo2/Ejer19.cs

--- teoo2/Ejer19.cs
+++
@@ -1,8 +0,0 @@
-public class Ejer19
-{
-    public static void Ejecutar()
-    {
-        //Idem. al ejercicio 18.a) y 18.b) pero devolviendo el resultado en un parámetro de salida void
-        //Fac(int n, out int f)
-    }
-}
